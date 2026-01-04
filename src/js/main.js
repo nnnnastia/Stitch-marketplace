2d67e413f6abf3163f74372e42745c3b885ef29d
Anastasiia <ntkach2006@gmail.com>
2026-01-04 22:14:57 +0200

    Add header

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,15 @@
-import "../sass/style.scss";
+import "/src/sass/style.scss";
+
+const burger = document.querySelector(".burger"),
+    close = document.querySelector(".header__menu-back"),
+    menu = document.querySelector(".header__menu");
+
+burger.addEventListener("click", () => {
+    menu.classList.add("header__menu_active");
+    document.body.style.overflow = "hidden";
+});
+
+close.addEventListener("click", () => {
+    menu.classList.remove("header__menu_active");
+    document.body.style.overflow = "";
+});
